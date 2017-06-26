6595ba1144ebd9ef98ec2a563b87fdc71af83c93
Kyoshin <194944@edu.p.lodz.pl>
2017-06-26 07:53:32 +0200

    Test 03 Green

--- src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Vm.java
@@ -4,4 +4,9 @@ package edu.iis.mto.serverloadbalancer;
  * Created by kyo on 26.06.2017.
  */
 public class Vm {
+    public int size;
+
+    public Vm(int size) {
+        this.size = size;
+    }
 }
